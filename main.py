9f498ceda9ba0928df393f29becacc9e0e2f2dac
CodingQuell <ryequellhunter@gmail.com>
2020-07-20 18:22:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+randlist1 = [10]
+for e in range (10):
+  r = random.randint(1,6)
+  randlist1.append(r)
+for e in randlist1:
+  print(randlist1[e])
+ # of iterations: 10
+ #1 = 
+ #2 = 
+ #3 = 
+ #4 = 10%
+ #5 = 
+ #6 = 
+
+randlist2 = [10]
+for g in range (1,6):
+  d = random.randint(1,100)
+  randlist2.append(d)
+ for g in randlist2:
+   print(randlist2[g])
